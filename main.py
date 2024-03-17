f4eae5b9c5355c40dd2a182d9c27cb97ede5756d
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-03-17 02:00:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#Jaunel Deans
+#September 29, 2023
+#In this code, we predicted when would happen if specific lines of codes were changed. Also, we analzyed the different aspects and parts of the code. 
+
+# Starter code
+
+num1 = 25
+num2 = 4
+print("Enter a number")
+num3 = int(input())
+
+total1 = num1 // num2
+total2 = num3 + num2
+total3 = total1 - total2
+
+
+# How many variables are used in the code?
+  # Answer: 6 variable are used in the code. 
+
+# Give the line number where an output statement is used.
+  # Answer: Line 9. print("Enter a number") will output the words, "Enter a number"
+
+# What symbol is used for variable assignment?
+  # Answer: (=) assignment operator 
+
+# What value is stored in total1 at the end of the program?
+  # Answer: The value in total1 by the end of the program is the product of the values in num1 and in num2. num1 has a value of 25 and num2 has a value of 4. Therefore, the total1 is assigned the product of 25 and 4 which is 100. 
+
+# What value is stored in num3 at the end of the program?
+  # Answer: num3 will have the value of the whole number/integer that the user inputted. 
+
+# Line 8 is changed to total1 = num1 // num2. How would it affect the program?
+  # Answer: total1 would be assigned the quotient of the values in num1 and in num2. num1 has a value of 25 and num2 has a value of 4. Therefore, the total1 is assigned the quoient of 25 and 4 which is 6. Therefore, the value in total1 will change to the value of total2 from 6.  
